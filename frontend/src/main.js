5c1c842a1b62d7346a93cbf91bc8bfdd2bf8fa91
Liu Hang <LiuHang@KaKa888.Com>
2021-04-30 11:54:02 +0800

    Google Authencation Intergraded
    Signed-off-by: Liu Hang <LiuHang@KaKa888.Com>

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,8 +3,33 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vant from 'vant';
+import { Lazyload } from 'vant';
+import { Locale } from 'vant';
+import 'vant/lib/index.css';
+import 'vant/lib/icon/local.css'
+import enUS from 'vant/es/locale/lang/en-US';
+import axios from 'axios'
 
 Vue.config.productionTip = false
+
+Vue.use(Vant);
+Vue.use(Lazyload, {
+  lazyComponent: true,
+});
+Locale.use('en-US', enUS);
+
+Vue.prototype.$axios = axios;
+
+router.beforeEach((to, from, next) => {
+  // chrome
+  document.body.scrollTop = 0
+  // firefox
+  document.documentElement.scrollTop = 0
+  // safari
+  window.pageYOffset = 0
+  next()
+});
 
 /* eslint-disable no-new */
 new Vue({
