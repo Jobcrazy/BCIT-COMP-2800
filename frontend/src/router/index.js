c748e8d6be103c514c76bb415c36ae678f7cc739
Liu Hang <LiuHang@KaKa888.Com>
2021-04-30 12:04:46 +0800

    Added main tab
    Signed-off-by: Liu Hang <LiuHang@KaKa888.Com>

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/Login'
+import User_Main from '@/components/user/Main'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/user/main',
+      name: 'User_Main',
+      component: User_Main
     }
   ]
 })
