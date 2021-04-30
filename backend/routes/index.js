1c1519a4c88e91b9d3109e862e83f438fde3911c
Liu Hang <LiuHang@KaKa888.Com>
2021-04-30 11:06:00 +0800

    Remove redundant files
    Signed-off-by: Liu Hang <LiuHang@KaKa888.Com>

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
+var mysql      = require('mysql');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let obj = {
+    id: 1,
+    name: "Victor"
+  };
+
+  res.send(obj);
+});
+
+router.get('/two', function(req, res, next) {
+  res.send("Hello Two!")
+});
+
+router.get('/test', function(req, res, next) {
+  var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'victor',
+    password : 'goodboy',
+    database : 'victor_database'
+  });
+
+  // connect
+  connection.connect();
+
+  // get data(structural query language)
+  connection.query('SELECT * FROM user', function (error, results, fields) {
+    if (error) {
+      res.send("error occurred");
+      return;
+    };
+
+    res.send(results);
+  });
+
 });
 
 module.exports = router;
