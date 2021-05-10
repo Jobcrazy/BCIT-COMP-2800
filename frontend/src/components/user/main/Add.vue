307a83bd4d4064e467a90011938051ef219de0f7
Victor Li <vli30@my.bcit.ca>
2021-05-10 16:19:10 -0700

    Added forms for post creation page

--- frontend/src/components/user/main/Add.vue
+++ frontend/src/components/user/main/Add.vue
@@ -1,26 +1,64 @@
 <template>
   <div>
-    Add
+    <van-form @submit="onSubmit">
+      <van-field
+        v-model="title"
+        name="Title"
+        label="Title"
+        placeholder="Enter Title"
+        :rules="[{ required: true, message: 'Title is required' }]"
+      />
+      <van-field
+        v-model="description"
+        type="textarea"
+        name="Description"
+        label="Description"
+        placeholder="Enter Description"
+        rows="5"
+        autosize
+        maxlength="1000"
+        show-word-limit
+        :rules="[{ required: true, message: 'A description is required' }]"
+      />
+      <van-field
+        v-model="deposit"
+        type="number"
+        name="deposit"
+        label="Deposit"
+        placeholder="Enter the deposit amount"
+        :rules="[{ required: true, message: 'A deposit amount is required' }]"
+      />
+      <van-field
+        v-model="fee"
+        type="number"
+        name="fee"
+        label="Fee"
+        placeholder="Enter the fee amount"
+        :rules="[{ required: true, message: 'A fee amount is required' }]"
+      />
+      <div style="margin: 16px;">
+        <van-button round block type="info" native-type="submit">
+          Submit
+        </van-button>
+      </div>
+    </van-form>
   </div>
 </template>
 
 <script>
 export default {
-  name: "User_Main_Add",
   data() {
     return {
-      title: "Title",
-      description: "description",
-      deposit: "0",
-      fee: "0",
+      title: "",
+      description: "",
+      deposit: "",
+      fee: "",
     };
   },
   methods: {
-    createPost() {
-      asd
-    }
-  }
-  mounted() {
+    onSubmit(values) {
+      console.log('submit', values);
+    },
   },
 };
 </script>
