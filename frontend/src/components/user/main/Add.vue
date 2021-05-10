6eccbd1d2c717f3475c935ed73668c5336486ce6
Victor Li <vli30@my.bcit.ca>
2021-05-10 14:04:51 -0700

    test

--- frontend/src/components/user/main/Add.vue
+++ frontend/src/components/user/main/Add.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
-    <van-uploader
-      v-model="imageList"
-      multiple
-      :after-read="uploadImage"
-      :max-count="6"
-    />
+    Add
   </div>
 </template>
 
@@ -14,55 +9,19 @@ export default {
   name: "User_Main_Add",
   data() {
     return {
-      imageList: [],
+      title: "Title",
+      description: "description",
+      deposit: "0",
+      fee: "0",
     };
   },
   methods: {
-    uploadImage(file) {
-      let self = this;
-
-      // Set status
-      file.status = "uploading";
-      file.message = "Uploading...";
-
-      // Upload file
-      let form = new FormData();
-      form.append("file", file.file);
-
-      self
-        .$axios({
-          method: "POST",
-          headers: { "Content-Type": "multipart/form-data" },
-          url: "/api/file/upload",
-          data: form,
-        })
-        .then((res) => {
-          if (1 == res.data.code) {
-            // Not login
-            self.$toast.clear();
-            self.$router.push({ name: "Login" });
-            return;
-          } else if (0 != res.data.code) {
-            // Other errors
-            file.status = "failed";
-            file.message = "Failed";
-            return;
-          }
-
-          // Done uploading
-          file.status = "done";
-
-          // Set the unique id for this file
-          file.id = res.data.data[0].id;
-
-          console.log(res.data.data[0]);
-        })
-        .catch(function (error) {
-          self.$toast.fail(error);
-        });
-    },
+    createPost() {
+      asd
+    }
+  }
+  mounted() {
   },
-  mounted() {},
 };
 </script>
 
