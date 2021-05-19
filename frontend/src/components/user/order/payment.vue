e3afdfd6db901f3a9ab6582ad578e1c214ae3ee0
Victor Li <vli30@my.bcit.ca>
2021-05-18 17:30:28 -0700

    Added radio options for payment

--- frontend/src/components/user/order/payment.vue
+++ frontend/src/components/user/order/payment.vue
@@ -7,9 +7,29 @@
       fixed
       @click-left="onClickLeft"
     />
-
   </div>
+
+  <van-radio-group v-model="radio">
+    <van-cell-group>
+      <van-cell title="Paypal" clickable @click="radio = 'Paypal'">
+        <template #right-icon>
+          <van-radio name="Paypal" />
+        </template>
+      </van-cell>
+      <van-cell title="Credit Card" clickable @click="radio = 'Credit Card'">
+        <template #right-icon>
+          <van-radio name="Credit Card" />
+        </template>
+      </van-cell>
+      <van-cell title="Bike2Go Credit" clickable @click="radio = 'Bike2Go Credit'">
+        <template #right-icon>
+          <van-radio name="Bike2Go Credit" />
+        </template>
+      </van-cell>
+    </van-cell-group>
+  </van-radio-group>
 </template>
+
 
 <script>
 export default {
