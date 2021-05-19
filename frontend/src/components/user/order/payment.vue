956871f7b64324244a5d0f86916dce52488c59f1
Victor Li <vli30@my.bcit.ca>
2021-05-18 17:33:10 -0700

    Added submit button

--- frontend/src/components/user/order/payment.vue
+++ frontend/src/components/user/order/payment.vue
@@ -9,31 +9,53 @@
     />
   </div>
 
-  <van-radio-group v-model="radio">
-    <van-cell-group>
-      <van-cell title="Paypal" clickable @click="radio = 'Paypal'">
-        <template #right-icon>
-          <van-radio name="Paypal" />
-        </template>
-      </van-cell>
-      <van-cell title="Credit Card" clickable @click="radio = 'Credit Card'">
-        <template #right-icon>
-          <van-radio name="Credit Card" />
-        </template>
-      </van-cell>
-      <van-cell title="Bike2Go Credit" clickable @click="radio = 'Bike2Go Credit'">
-        <template #right-icon>
-          <van-radio name="Bike2Go Credit" />
-        </template>
-      </van-cell>
-    </van-cell-group>
-  </van-radio-group>
+  <van-form @submit="onSubmit">
+    <van-radio-group v-model="radio">
+      <van-cell-group>
+        <van-cell title="Paypal" clickable @click="radio = 'Paypal'">
+          <template #right-icon>
+            <van-radio name="Paypal" />
+          </template>
+        </van-cell>
+        <van-cell title="Credit Card" clickable @click="radio = 'Credit Card'">
+          <template #right-icon>
+            <van-radio name="Credit Card" />
+          </template>
+        </van-cell>
+        <van-cell title="Bike2Go Credit" clickable @click="radio = 'Bike2Go Credit'">
+          <template #right-icon>
+            <van-radio name="Bike2Go Credit" />
+          </template>
+        </van-cell>
+      </van-cell-group>
+    </van-radio-group>
+
+    <div style="margin: 16px;">
+      <van-button round block type="info" native-type="submit">
+        Submit
+      </van-button>
+    </div>
+  </van-form>
+
+
 </template>
 
 
 <script>
 export default {
-  mounted() {},
+  data() {
+    return {
+      username: '',
+      password: '',
+    };
+  },
+  methods: {
+    onSubmit(values) {
+      console.log('submit', values);
+    },
+  },
+
+  mount() {},
 };
 </script>
 
