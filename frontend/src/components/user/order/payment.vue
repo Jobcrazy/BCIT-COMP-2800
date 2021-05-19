2f42d0de7b57b2a45f183972e2b087879695c284
Victor Li <vli30@my.bcit.ca>
2021-05-18 17:17:41 -0700

    Added top nav bar

---
+++ frontend/src/components/user/order/payment.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="this_page">
+    <van-nav-bar
+      title="Payment"
+      left-text="Back"
+      left-arrow
+      fixed
+      @click-left="onClickLeft"
+    />
+
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+#this_page {
+  padding-top: 46px;
+  padding-bottom: 20px;
+}
+</style>
