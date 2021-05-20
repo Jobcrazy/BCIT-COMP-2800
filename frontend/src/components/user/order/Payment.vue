b715d15987157de5f9ad7d4cf53ddc4a39fb029e
Victor Li <vli30@my.bcit.ca>
2021-05-20 13:19:48 -0700

    temp push

--- frontend/src/components/user/order/Payment.vue
+++ frontend/src/components/user/order/Payment.vue
@@ -7,87 +7,134 @@
       fixed
       @click-left="onClickLeft"
     />
-  </div>
+    <van-collapse v-model="activeName" accordion>
+      <van-collapse-item title="Credit Card" name="1">
+        <van-form @submit="onSubmit">
+          <van-field
+            v-model="dataToSubmit.cardNumber"
+            name="cardNumber"
+            label="Credit Card Number"
+            placeholder="Credit Card Number"
+            :rules="[{ required: true, message: 'Credit card number is required' }]"
+          />
+          <van-field
+            v-model="dataToSubmit.fName"
+            name="Cardholder Name"
+            label="Cardholder Name"
+            placeholder="Cardholder name"
+            :rules="[{ required: true, message: 'Name is required' }]"
+          />
+          <van-datetime-picker id="datePicker"
+                               v-model="dataToSubmit.currentDate"
+                               type="year-month"
+                               title="Enter expiry date"
+                               :min-date="minDate"
+                               :max-date="maxDate"
+                               :formatter="formatter"
+          />
+          <van-password-input id="cvcInput"
+                              info="Enter CVC (Back of card)"
+                              :value="dataToSubmit.cvc"
+                              :gutter="2"
+                              :length="3"
+                              :mask="false"
+                              :focused="showKeyboard"
+                              @focus="showKeyboard = true"
+          />
+          <van-number-keyboard
+            v-model="dataToSubmit.cvc"
+            :show="showKeyboard"
+            @blur="showKeyboard = false"
+          />
+        </van-form>
+      </van-collapse-item>
+      <van-collapse-item title="Paypal" name="2">Paypal API to be implemented</van-collapse-item>
+      <van-collapse-item title="Bik2Go Credit" name="3">Unavailable</van-collapse-item>
+    </van-collapse>
 
-  <van-form @submit="onSubmit">
-    <van-radio-group v-model="radio">
-      <van-cell-group>
-        <van-cell title="Paypal" clickable @click="radio = 'Paypal'">
-          <template #right-icon>
-            <van-radio name="Paypal" />
-          </template>
-        </van-cell>
-        <van-cell title="Credit Card" clickable @click="radio = 'Credit Card'">
-          <template #right-icon>
-            <van-radio name="Credit Card" />
-          </template>
-        </van-cell>
-        <van-cell title="Bike2Go Credit" clickable @click="radio = 'Bike2Go Credit'">
-          <template #right-icon>
-            <van-radio name="Bike2Go Credit" />
-          </template>
-        </van-cell>
-      </van-cell-group>
-    </van-radio-group>
-
-    <div style="margin: 16px;">
-      <van-button round block type="info" native-type="submit">
+    <div style="margin: 16px">
+      <van-button round block type="info" native-type="submit" @left-click:>
         Submit
       </van-button>
     </div>
-  </van-form>
 
-
+  </div>
 </template>
-
 
 <script>
 export default {
   data() {
     return {
+      show: false,
+      value: '',
+      showKeyboard: false,
+      activeName: '1',
+      minDate: new Date(2020, 0, 1),
+      maxDate: new Date(2025, 10, 1),
       dataToSubmit: {
-        paymentMethod: "",
+        cardNumber: '1345',
+        fName: 'Victor',
+        currentDate: new Date(),
+        cvc: '',
       },
     };
   },
   methods: {
-    onClickLeft(){
+    onClickLeft() {
       history.back();
     },
+    formatter(type, val) {
+      if (type === 'year') {
+        return `${val} Year`;
+      } else if (type === 'month') {
+        return `${val} Month`;
+      }
+      return val;
+    },
     onSubmit(values) {
-      console.log('submit', values);
+      console.log("submit", values);
       this.$axios({
         method: "POST",
-        url: "/api/order/payment",
+        url: "/api/order/make",
         data: this.dataToSubmit,
       })
         .then((res) => {
           if (1 == res.data.code) {
             self.$toast.clear();
-            self.$router.push({ paymentMethod: "something here?"});
+            self.$router.push({ name: "Login" });
             return;
           } else if (0 != res.data.code) {
             self.$toast.fail(res.data.message);
             return;
           }
 
-          self.$router.push({ paymentMethod: "something here?"});
+          self.$router.push({ name: "Order_List" });
         })
-        .catch(function (error){
+        .catch(function (error) {
           self.$toast.fail(error);
         });
     },
   },
 
-  mount() {},
+  mounted() {
+
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 #this_page {
   padding-top: 46px;
   padding-bottom: 20px;
+}
+#datePicker {
+  margin-top: 7%;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+#cvcInput {
+  margin-top: 7%;
+  background-color:lightgrey;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 }
 </style>
