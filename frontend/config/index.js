2401100b9636aada6dc8e8fd5fcca30891eef08b
James Liu <LiuHang@KaKa888.Com>
2021-05-22 04:13:17 +0800

    Bug fix
    Signed-off-by: James Liu <LiuHang@KaKa888.Com>

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -12,8 +12,8 @@ module.exports = {
     proxyTable: {
       "/": {
         changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
-        //target: "http://bike.kaka888.net", // 接口的域名
-        target: "http://localhost:3000", // 接口的域名
+        target: "http://bike.kaka888.net", // 接口的域名
+        //target: "http://localhost:3000", // 接口的域名
         pathRewrite: {
           "^": "" //后面可以使重写的新路径，一般不做更改
         }
